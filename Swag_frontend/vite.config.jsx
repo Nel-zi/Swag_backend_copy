d916f83e5951979acf0d7a6a54aee2ef90cfd096
Nel-zi <nelxzxi@gmail.com>
2025-07-05 07:49:05 +0100

    Update to vite.config and requirements.txt

--- Swag_frontend/vite.config.jsx
+++ Swag_frontend/vite.config.jsx
@@ -3,10 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',          // ← add this
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false
-    }
-  }
+  server: { hmr: { overlay: false } }
 });
